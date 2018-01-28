f512f34b556c6f13a0cd46a8f25f36b31696b00a
idekPL <pawelidryjan@poczta.fm>
2018-01-28 17:46:31 +0100

    main1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <cstdlib>
+#include "dodawanie.cpp"
+#include "odejmowanie.cpp"
+#include "mnozenie.cpp"
+#include "dzielenie.cpp"
 
 using namespace std;
 double a, b;
+
 int main()
 {
+	
 cout<<"Podaj a: "; 	
 cin>>a;
 cout<<endl<<"Podaj b: ";	
 cin>>b;
-
 
 	
 	
