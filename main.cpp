09d0f67170b7e609e07e9bb0e0e4df3ef374a01f
idekPL <pawelidryjan@poczta.fm>
2018-01-28 16:50:51 +0100

    main_poczatek

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+double a, b;
+int main()
+{
+cout<<"Podaj a: "; 	
+cin>>a;
+cout<<endl<<"Podaj b: ";	
+cin>>b;
+
+
+	
+	
+system("PAUSE");
+return 0;	
+}
