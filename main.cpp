252e9dac64b5c165d40de00f668a4d589ab9608e
idekPL <pawelidryjan@poczta.fm>
2018-01-28 17:57:20 +0100

    main2

--- main.cpp
+++ main.cpp
@@ -4,19 +4,50 @@
 #include "odejmowanie.cpp"
 #include "mnozenie.cpp"
 #include "dzielenie.cpp"
+#include <math.h>
 
 using namespace std;
 double a, b;
+int wybor;
 
 int main()
 {
-	
+cout<<"LAborka 6 - kalkulator"<<endl;
+cout<<"Wybierz opcjê: 1 - dodawanie; 2 - odejmowanie; 3 - mno¿enie; 4 - dzielenie"<<endl;
+cin>>wybor;	
 cout<<"Podaj a: "; 	
 cin>>a;
 cout<<endl<<"Podaj b: ";	
 cin>>b;
 
+switch(wybor)
+	{
+	case 1:
+		{
+			
+		}
 	
+	case 2:
+		{
+			
+		}	
+	
+	case 3:
+		{
+			
+		}
+		
+	case 4:
+		{
+			
+		}	
+	
+	case default:
+		{
+		cout<<"Z³y wybór opcji!!!"<<endl;	
+		}		
+	}
+cout<<"Koniec programu";	
 	
 system("PAUSE");
 return 0;	
