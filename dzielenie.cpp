18a686b7f564b7af0aadbc069494034d8e0041ea
anteczko <s21@onet.pl>
2018-02-04 15:32:38 +0100

    przy dzieleniu przez zero fukcja bedzei zwracala -1

--- dzielenie.cpp
+++ dzielenie.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+
 double dzielenie (double a,double b){
-	if(b==0)std::cout<<"nie dzielimy przez zero!"<<endl;
+	if(b==0){
+		std::cout<<"nie dzielimy przez zero!"<<endl;
+		return -1;
+	}
 	return 	a/b;
 }
