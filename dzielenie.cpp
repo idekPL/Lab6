aa7e41fcf34d04da7584aa278d03a78557cd9715
anteczko <s21@onet.pl>
2018-02-04 15:38:36 +0100

    lepszy przejrzysty kod, usunieta niepotrzebne importowanie bibliotek tkrore i tak plik glowny zimportuje

--- dzielenie.cpp
+++ dzielenie.cpp
@@ -1,13 +1,9 @@
-#include <iostream>
-
-using namespace std;
-
 double dzielenie (double a,double b){
 	if(b==0){
-		cout<<"nie dzielimy przez zero!"<<endl;
+		std::cout<<"nie dzielimy przez zero!"<<endl;
 		return -1;
 	}
-	return 	a/b;
+	else return a/b;
 }
 
 /*
